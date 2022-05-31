a77415c5909e2633d098ab26df4382802bc9a2aa
Tala-coder <muhammedtalic.it@gmail.com>
2022-05-31 18:40:53 +0200

    Create AppDBContext.cs

---
+++ aspnetserver2/aspnetserver2/Data/AppDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aspnetserver2.Data
+{
+    internal sealed class AppDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder.UseSqlite("Data Source=./Data/AppDB.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Post[] postsToSeed = new Post[6];
+
+            for (int i = 1; i <= 6; i++)
+            {
+                postsToSeed[i - 1] = new Post
+                {
+                    PostId = i,
+                    Naslov_oglasa = $"Post {i}",
+                    Naziv_firme = $"Naziv_firme {i}",
+                    Lokacija = $"Lokacija {i}",
+                    Iskustvo = $"Iskustvo {i}",
+                    Apliciraj_link = $"Apliciraj_link {i}",
+                    Slika_firme = $"Slika_firme {i}" 
+                 };
+            }
+
+            modelBuilder.Entity<Post>().HasData(postsToSeed);
+        }
+    }
+}
